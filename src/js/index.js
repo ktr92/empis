7448dcb70f2d5b3b727199a0a1824c2a2e5a7174
ktr92 <kudashevtim@gmail.com>
2021-11-01 17:22:19 +0300

    app

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,31 @@
+const {
+  eventListeners
+} = require("@popperjs/core");
+
 document.addEventListener('DOMContentLoaded', function () {
+
+  let cover = document.querySelector('.pageheader--cover')
+  if (cover) {
+    document.getElementById('header').classList.add('header--white')
+  }
+
   document.querySelector('.header__menu a').addEventListener('click', (event) => {
     event.preventDefault()
     document.getElementById('topmenu').classList.add('active')
+  })
+
+
+
+  document.querySelector('.contact__call').addEventListener('click', (event) => {
+
+    if (event.currentTarget.classList.contains('active')) {
+      event.currentTarget.classList.remove('active')
+      document.querySelector('.contact__content').classList.remove('active')
+    } else {
+      event.currentTarget.classList.add('active')
+      document.querySelector('.contact__content').classList.add('active')
+    }
+
   })
 
   document.querySelector('.topmenu__close a').addEventListener('click', (event) => {
@@ -21,7 +45,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
     });
   } else {
-    document.getElementById('header').classList.add('fixed')
+    window.addEventListener('scroll', (event) => {
+      scroll = window.scrollY
+      if (scroll >= 177) {
+        document.getElementById('header').classList.add('fixed')
+      } else {
+        document.getElementById('header').classList.remove('fixed')
+      }
+
+    });
   }
 
 
