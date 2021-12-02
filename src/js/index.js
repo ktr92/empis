47c90892e51ea7d9cdaf37910ad82c25323b96f7
ktr92 <kudashevtim@gmail.com>
2021-12-02 20:02:19 +0300

    landing

--- src/js/index.js
+++ src/js/index.js
@@ -149,6 +149,26 @@ $(document).ready(function () {
   });
 
 
+  $('#slider-devblock').sly({
+    horizontal: 1,
+    itemNav: 'forceCentered',
+    smart: 1,
+    activateMiddle: 1,
+    mouseDragging: 1,
+    touchDragging: 1,
+    releaseSwing: 1,
+    startAt: 0,
+    scrollBy: 1,
+    speed: 300,
+    elasticBounds: 1,
+    dragHandle: 1,
+    dynamicHandle: 1,
+    clickBar: 1,
+
+    next: $(this).find('.next')
+  });
+
+
   $("input[type=tel]").mask("+7 (999) 999 99 99");
 
   $("a.scrollTo").click(function () {
